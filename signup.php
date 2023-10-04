8162b588a108f64a408ecf977227433ff0c31e33
Preeti <preeti.srivastava.in@gmail.com>
2023-10-04 17:45:02 +0530

    signup page

--- signup.php
+++ signup.php
@@ -1,3 +1,25 @@
+<?php
+$showAlert = false;
+$showError = false;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  include 'partials/_dbconnect.php';
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $cpassword = $_POST["cpassword"];
+  $exsits=false;
+  if(($password == $cpassword) && ($exsits==false)){
+   $sql = "INSERT INTO `users` (`username`, `password`, `dt`) VALUES ('$username', '$password', current_timestamp())";
+   $result = mysqli_query($conn, $sql); 
+   if($result){
+    $showAlert = true;
+   }
+  }
+  else{
+    $showError = "Password not match";
+  }
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,13 +33,27 @@
     <title>Signup</title>
   </head>
   <body>
-    <?php require 'partials/_nav.php'?>
+    <?php require 'partials/_nav.php';?>
+    <?php
+    if($showAlert){
+      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Your account is now created and you can login.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+    }
+    if($showError){
+      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Error!</strong> '.$showError.'
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+    }
+    ?>
     <div class="container">
         <h1 class="text-center">Signup to our website</h1>
         <form action="/loginsystem/signup.php" method="post">
             <div class="form-group">
                 <label for="username">Username</label>
-                <input type="email" class="form-control" id="username" name="username" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp" placeholder="Enter email">
                 
             </div>
             <div class="form-group">
@@ -28,7 +64,7 @@
                 <label for="cpassword">Confirm Password</label>
                 <input type="password" class="form-control" id="cpassword" name="cpassword" placeholder="Password">
                 <small id="emailHelp" class="form-text text-muted">Make sure to type the same password</small>
-            </div>
+            </div>x
             <button type="submit" class="btn btn-primary">Signup</button>
         </form>
 
